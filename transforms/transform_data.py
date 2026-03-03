9f84b0a68e98b26bdc1fbc90b56f0be06e8e7047
JJ <jakesjordan00@gmail.com>
2026-03-02 22:29:48 -0500

    small upadte to transform

--- transforms/transform_data.py
+++ transforms/transform_data.py
@@ -47,23 +47,24 @@ class Transform:
                 'HomeTeam': g['homeTeam'],
                 'AwayTeam': g['awayTeam'],
                 }             
-            for g in data if g['gameStatus'] != 1]
+            for g in data]
         
         #add T+10m to tipoff as a condition
         not_started = [game for game in scoreboard if game['GameStatus'] == 1] 
         log_ns = f'{len(not_started)} games yet to start' if len(not_started) != 1 else f'{len(not_started)} game yet to start'
-        log_ns += f'{log_ns}: {', '.join(str(game['GameID']) for game in not_started)}'
+        log_ns = f'{log_ns}: {', '.join(str(game['GameID']) for game in not_started)}'
         self.logger.info(log_ns)
 
         in_progress = [game for game in scoreboard if game['GameStatus'] == 2]
         log_ip = f'{len(in_progress)} games are in progress' if len(in_progress) != 1 else f'{len(in_progress)} game is in progress'
-        log_ip += f'{log_ip}: {', '.join(str(game['GameID']) for game in in_progress)}'
+        log_ip = f'{log_ip}: {', '.join(str(game['GameID']) for game in in_progress)}'
         self.logger.info(log_ip)
 
         completed = [game for game in scoreboard if game['GameStatus'] == 3]
         log_c = f'{len(completed)} games have concluded' if len(completed) != 1 else f'{len(completed)} game has concluded'
-        log_c += f'{log_c}: {', '.join(str(game['GameID']) for game in completed)}'
+        log_c = f'{log_c}: {', '.join(str(game['GameID']) for game in completed)}'
         self.logger.info(log_c)
-
-        return scoreboard
+        
+        games = [game for game in scoreboard if game['GameStatus'] != 1] 
+        return games
 
