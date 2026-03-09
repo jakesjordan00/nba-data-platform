c6d878672c7ed3e8782994b5f1ba536ad249c5c7
JJ <jakesjordan00@gmail.com>
2026-03-09 11:09:09 -0400

    scheduler

--- dags/daily_lineups.py
+++ dags/daily_lineups.py
@@ -8,7 +8,7 @@ import textwrap
     dag_id = 'daily_lineups_pipeline',
     dag_display_name = 'Daily Lineups Pipeline',
     start_date = datetime(year=2026, month=3, day=1),
-    schedule = '*/15 12-22 * 10-12, 1-5 *',
+    schedule = '*/15 12-22 * 10-12,1-5 *',
     catchup = False,
     max_active_runs = False,
     default_args = {
